8e87bfdc8d464e91967ec918c7ca549b979a3890
Veasna <Veasna3d@gmail.com>
2023-05-21 22:52:09 +0700

    Get User

--- resources/js/components/pages/User.vue
+++ resources/js/components/pages/User.vue
@@ -1,3 +1,20 @@
+<script setup>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const users = ref([]);
+
+const getUsers = () =>{
+    axios.get('/api/users').then((response) =>{
+        users.value = response.data
+    })
+}
+
+onMounted(() => {
+    getUsers();
+});
+</script>
+
 <template>
     <div class="content-header">
         <div class="container-fluid">
@@ -18,8 +35,31 @@
 
     <div class="content">
         <div class="container-fluid">
-       
+            <div class="card">
+                <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Registered Date</th>
+                        <th scope="col">Role</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="user in users" :key="user.id">
+                        <th scope="row">{{ user.id }}</th>
+                        <td>{{ user.name }}</td>
+                        <td>{{ user.email }}</td>
+                        <td>-</td>
+                        <td>-</td>
+                        <td>-</td>
+                    </tr>
 
+                </tbody>
+            </table>
+            </div>
         </div>
     </div>
 </template>
